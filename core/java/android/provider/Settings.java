339be7548d3f541059250a3564391149116abcc5
tsubus <tsubus@ilwt.org>
2014-12-05 20:47:49 +0100

    Base: Implement ad blocker [2/3]
    *Hosts File Manager
    *Picked from Dirty Unicorns
    *This uses two prebuilt hosts files and switches between them by symlinking
    
    Change-Id: I23a1d9f1dcca623a556defb5eaae94f4e9e3b21d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3835,6 +3835,13 @@ public final class Settings {
         public static final String DIALER_OPENCNAM_AUTH_TOKEN = "dialer_opencnam_auth_token";
 
         /**
+         * Base: Implement ad blocker [2/3]
+	 * Disable ads (HFM)
+	 * @hide
+	 */
+	public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
